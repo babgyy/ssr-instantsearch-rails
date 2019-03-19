855c41dcec069a588903c6a23a99fd10ea1b3a6d
Lucas Babey <babeylucas@gmail.com>
2019-03-19 18:58:25 +0100

    Wired React Instantsearch. Index is Post data from seed

--- app/javascript/components/PostSearch.js
+++ app/javascript/components/PostSearch.js
@@ -1,8 +1,24 @@
 import React from "react";
+// import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
+
+// const searchClient = algoliasearch(
+//   "latency",
+//   "6be0576ff61c053d5f9a3225e2a90f76"
+// );
 
 class PostSearch extends React.Component {
   render() {
-    return <p>{this.props.data}</p>;
+    return (
+      <InstantSearch
+        appId="0S51WQJHQ7"
+        apiKey="dde8a3eb27c99e55dede6d2c8ab0c6dd"
+        indexName="Post"
+      >
+        <SearchBox />
+        <Hits />
+      </InstantSearch>
+    );
   }
 }
 
